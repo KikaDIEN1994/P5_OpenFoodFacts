55309a57a02dfbfc8ef6151a6eed3eb37d42ea9b
KikaDIEN1994 <dien.kika@gmail.com>
2022-05-20 13:51:17 +0200

    add database.py

--- database.py
+++ database.py
@@ -1,30 +1,43 @@
-from peewee import *
 from tqdm import tqdm
 import model
 
+
 class Database:
-   def __init__(self, products):
-       self.products = products
-       
+    #  init database product
+    def __init__(self, products):
+        self.products = products
 
-   def fill_database(self):
-      self.fill_product()
+    def fill_database(self):
+        self.fill_product()
 
-   def fill_product(self):
-      print("Product inserting ...")
-      for product in tqdm(self.products):
-         model.Product.get_or_create(name=product.name, nutriscore=product.nutriscore,
-            stores=product.stores, url=product.url)
-         product_selected = model.Product.select().where(model.Product.name == product.name, model.Product.nutriscore == product.nutriscore)
-         for category in product.categories:
-            self.fill_category(category)
-            category_selected = model.Category.select().where(model.Category.name_category ==category)
-            self.fill_categorized(product_selected, category_selected)
+    # fill database product and category with get_or_create
+    def fill_product(self):
+        print("Product inserting ...")
+        for product in tqdm(self.products):
+            model.Product.get_or_create(
+                name=product.name,
+                nutriscore=product.nutriscore,
+                stores=product.stores,
+                url=product.url,
+            )
+            product_selected = model.Product.select().where(
+                model.Product.name == product.name,
+                model.Product.nutriscore == product.nutriscore,
+                model.Product.url == product.url,
+            )
+            print(product.name)
+            for category in product.categories:
+                self.fill_category(category)
+                category_selected = model.Category.select().where(
+                    model.Category.name_category == category
+                )
+                self.fill_categorized(product_selected, category_selected)
 
-   def fill_category(self, category):
-      model.Category.get_or_create(name_category=category)
+    def fill_category(self, category):
+        model.Category.get_or_create(name_category=category)
 
-   def fill_categorized(self, product, category):
-      model.Categorized.get_or_create(product_fk=product, category_fk=category)
-
-   
+    # fill database of categorized
+    def fill_categorized(self, product, category):
+        if len(product) > 1:
+            product = product[0]
+        model.Categorized.get_or_create(product_fk=product, category_fk=category)
